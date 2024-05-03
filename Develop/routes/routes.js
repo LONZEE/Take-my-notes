369aff30b72b788408536d3e3392e3e4908a179a
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-03 16:04:13 -0500

    problem with path

---
+++ Develop/routes/routes.js
@@ -0,0 +1,39 @@
+const routes = require('express').Router();
+const fs = require('fs');
+const uuid  = require('uuid');
+const readDbJson = () => JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+
+// Function to write to db.json file
+const writeDbJson = (data) => {
+  const jsonData = JSON.stringify(data);
+  fs.writeFileSync("db/db.json", jsonData);
+};
+
+// API routes
+routes.get('/api/notes', (req, res) => {
+  const dbJson = readDbJson();
+  res.json(dbJson);
+});
+
+routes.post('/api/notes', (req, res) => {
+  const dbJson = readDbJson();
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuid(),
+  };
+  dbJson.push(newNote);
+  writeDbJson(dbJson);
+  res.json(dbJson);
+});
+
+// HTML routes
+routes.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+routes.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = routes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const routes = require('./routes/routes');
+app.use(express.json());
 app.use(express.static('public'));
+app.use(routes);
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
